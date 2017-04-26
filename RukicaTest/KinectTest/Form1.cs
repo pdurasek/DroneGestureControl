bd0bf7847ec71482cf880c5b9de7d56a74556003
Patrik Durasek <pxd7081@rit.edu>
2017-04-26 17:16:50 +0200

    Added solution to control the robotic hand

--- RukicaTest/KinectTest/Form1.cs
+++ RukicaTest/KinectTest/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,7 @@ namespace KinectTest
         private Body[] bodies = null;
         private Timer gestureTimer = null;
         private bool initialScan = false;
-        private TimeSpan cycleSpan = new TimeSpan(0, 0, 0, 0, 250);
+        private TimeSpan cycleSpan = new TimeSpan(0, 0, 0, 0, 200);
         private HandCoordinate handCoordinate = null;
         private bool isInMotion = false;
         private float changeSum = 0;
@@ -32,11 +33,19 @@ namespace KinectTest
         // Hand origin locations
         private float rightHandOriginX, rightHandOriginY, rightHandOriginZ = 0;
 
+        SerialPort port = new SerialPort("COM4", 9600);
 
         public Form1()
         {
             InitializeComponent();
             InitializeKinect();
+            port.Open();
+            //port.DataReceived += (usb, a) =>
+            //{
+            //    byte[] buffer = new byte[port.BytesToRead];
+            //    port.Read(buffer, 0, port.BytesToRead);
+            //    Console.WriteLine(Encoding.UTF8.GetString(buffer));
+            //};            
         }
 
         public void InitializeKinect()
@@ -114,35 +123,8 @@ namespace KinectTest
                             {
                                 gestureTimer = new Timer(x =>
                                 {
-                                    bool isPalmUp;
-
-                                    // TODO Change at 75% rather than 50%
-                                    if (leftThumbCurrentX < leftHandCurrentX)
-                                    {
-                                        Console.WriteLine("Up");
-                                        isPalmUp = true;
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine("Down");
-                                        isPalmUp = false;
-                                    }
-                                    //Code for snapshots
-                                    //if (!isInMotion)
-                                    //{
-                                    //    rightHandOriginX = rightHandCurrentX; 
-                                    //    rightHandOriginY = rightHandCurrentY;
-                                    //    rightHandOriginZ = rightHandCurrentZ;
-                                    //}
-
-                                    if (isPalmUp)
-                                    {
-                                        CheckTurnOnMotion();
-                                    }
-                                    else
-                                    {
-                                        CheckTurnOffMotion();
-                                    }
+                                    //port.Write("[" + rightHandCurrentX + "," + rightHandCurrentY + "," + rightHandCurrentZ + "]");
+                                    calculado();
                                 }, null, 0, cycleSpan.Milliseconds);
 
                                 initialScan = true;
@@ -164,48 +146,46 @@ namespace KinectTest
             }
         }
 
-        private void CheckTurnOnMotion()
+        private void calculado()
         {
-            if (handCoordinate != null)
+            double currentX = rightHandCurrentX * 10;
+            double currentY = rightHandCurrentY * 10;
+            double currentZ = rightHandCurrentZ * 10;
+            double r = Math.Sqrt(Math.Pow(rightHandCurrentX, 2) + Math.Pow(rightHandCurrentY, 2) + Math.Pow(rightHandCurrentZ, 2)) * 10;
+            double bazniKut = Math.Acos(currentZ/r) *100;
+            double lakat = Math.Acos(currentY/r);
+            double rotacija = Math.Atan(currentX/r) * 100;
+            //Console.WriteLine("R: " + r);
+            //Console.WriteLine("Curent X: " + currentX + ", Current Y:" + currentY + ", Current Z: " + currentZ);
+            //Console.WriteLine("bazni: " + bazniKut);
+            //Console.WriteLine("lakat: " + lakat);
+            //Console.WriteLine("rotacija: " + rotacija);
+            //port.Write("[" + (int)rotacija + "," + (int)bazniKut + "," + (int)lakat + "]");
+           
+            //Console.WriteLine(MapValue(-40, 40, 15, 155, currentY));
+            lakat = MapValue(-3.5, 9, 80, 170, currentY, 80, 170);
+            
+            bazniKut = MapValue(10, 19, 150, 0, currentZ, 0, 150);
+            Console.WriteLine("bazni kut: " + bazniKut);
+            rotacija = MapValue(-4.5, 4.5, 155, 15, currentX, 15, 155);
+            Console.WriteLine("[" + (int)rotacija + "," + (int)bazniKut + "," + (int)lakat + "]");
+            port.Write("[" + (int)rotacija + "," + 90 + "," + 180 + "]");
+        }
+
+        public double MapValue(double a0, double a1, double b0, double b1, double a, int motorMin, int motorMax)
+        {
+            double rotus = b0 + (b1 - b0) * ((a - a0) / (a1 - a0));
+
+            if (rotus < motorMin)
             {
-                if (handCoordinate.oldThumbX < rightThumbCurrentX &&
-                    Math.Abs(handCoordinate.x - rightHandCurrentX) < ErrorMargin &&
-                    Math.Abs(handCoordinate.y - rightHandCurrentY) < ErrorMargin &&
-                    Math.Abs(handCoordinate.z - rightHandCurrentZ) < ErrorMargin)
-                {
-                    float xChange = Math.Abs(rightThumbCurrentX - handCoordinate.oldThumbX);
-                    if (xChange >= ErrorMargin)
-                    {
-                        isInMotion = true;
-                        changeSum += xChange;
-                    }
-                    else
-                    {
-                        isInMotion = false;
-                        changeSum = 0;
-                    }
-                }
-                else
-                {
-                    isInMotion = false;
-                    changeSum = 0;
-                }
+                rotus = motorMin;
+            }
+            if (rotus > motorMax)
+            {
+                rotus = motorMax;
             }
 
-            if (changeSum >= rangeOfMotion && isInMotion) //TODO Calculate hand - thumb * 2?
-            {
-                Console.WriteLine("Turned on");
-                sendMethod(statusLabel, "Trigger reached - Turning on the drone!");
-                sendMethod(engineStateLabel, "On");
-                changeSum = 0;
-                isInMotion = false;
-            }
-            else
-            {
-                sendMethod(statusLabel, "Waiting for trigger");
-            }
-            handCoordinate = new HandCoordinate(rightHandCurrentX, rightHandCurrentY, rightHandCurrentZ,
-                rightThumbCurrentX);
+            return rotus;
         }
 
         private void CheckTurnOffMotion()
@@ -285,4 +265,5 @@ namespace KinectTest
             this.oldThumbX = oldThumbX;
         }
     }
+
 }
